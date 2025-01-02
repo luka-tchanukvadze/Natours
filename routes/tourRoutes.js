e00a362ff58b5570023e04e3d2ae80524fcbce97
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-01-02 23:23:21 +0400

    let's make everything perfect

---
+++ routes/tourRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const tourController = require('./../controllers/tourController');
+
+const router = express.Router();
+
+router.param('id', tourController.checkID);
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
+
+router
+  .route('/:id')
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour)
+  .get(tourController.getTour);
+
+module.exports = router;
