8e7ba53f6acd41159c57b03def951ac2d2682f6a
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-01-05 21:44:34 +0400

    added tour modal and cleaned up some code

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
@@ -17,24 +16,6 @@ mongoose
   .then((con) => console.log('DB connection successful'));
 
 const app = require('./app');
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
