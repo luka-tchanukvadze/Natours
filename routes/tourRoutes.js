5f63590f8f3fd130dd1b7165ce12b01ec4f5c980
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-01-09 00:46:36 +0400

    problem

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,7 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+console.log('hey');
 
 router
   .route('/')
