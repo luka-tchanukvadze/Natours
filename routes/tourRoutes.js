eaf8a8d217e6a18aa0a9e4d30cd280577d7ff541
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-01-08 23:59:00 +0400

    Backdated commit for January 8th

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,7 +4,6 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
-console.log('hey');
 
 router
   .route('/')
