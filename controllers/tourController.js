dd6abfcf44419f1a90b464edc2b99caa1a83810e
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-01-10 22:12:13 +0400

    added pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -35,6 +35,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10 ... 1-10, page 1 ... 11-20, page2
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // Execute query
     const tours = await query;
 
