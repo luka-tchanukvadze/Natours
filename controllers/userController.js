d640b4e51a409da0bbe9f073dd3597ca210a2ae5
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-01-30 21:07:15 +0400

    building handler factory function delete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const AppError = require('../utils/appError');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 const filterOBJ = (obj, ...allowedFields) => {
   const newObj = {};
@@ -81,9 +82,4 @@ exports.updateUser = (req, res) => {
   });
 };
 
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
+exports.deleteUser = factory.deleteOne(User);
