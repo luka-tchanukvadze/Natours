5eb898df46ff865773905bca391d49b136e619fc
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-01-31 22:48:33 +0400

    Factory functions read one

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,18 +64,12 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined',
+    message: 'This route is not yet defined! Please use /signup instead',
   });
 };
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
+exports.getUser = factory.getOne(User);
 
 // Do NOT update passwords with this!
 exports.updateUser = factory.updateOne(User);
-
 exports.deleteUser = factory.deleteOne(User);
