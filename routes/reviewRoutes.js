1b8f8bbb55e76930c15874996b2e56d8c8e796e2
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-02-01 14:53:10 +0400

    Adding missing Authentication and Authorization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,11 +6,16 @@ const router = express.Router({
   mergeParams: true,
 });
 
+// POST /tour/fdksf34/reviews
+// GET /tour/fdksf34/reviews
+// POST /reviews
+
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -19,7 +24,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
