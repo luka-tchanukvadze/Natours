308ee570e224bfbd19c30741fada3467c47b9cc2
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-02-09 22:09:05 +0400

    loggin in users with api

--- public/js/login.js
+++ public/js/login.js
@@ -1,44 +1,49 @@
 /* eslint-disable */
 
-/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
   try {
-    const res = await fetch('http://127.0.0.1:3000/api/v1/users/login', {
+    const res = await axios({
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: { email, password },
     });
 
-    const data = await res.json(); // Convert response to JSON
-
-    if (!res.ok) {
-      throw new Error(data.message || 'Login failed');
-    }
-
-    if (data.status === 'success') {
-      alert('Logged in successfully!');
+    if (res.data.status === 'success') {
+      showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (error) {
-    alert(error.message || 'An error occurred');
+    showAlert('error', error.response.data.message);
   }
 };
+// export const login = async (email, password) => {
+//   try {
+//     const res = await fetch('http://127.0.0.1:3000/api/v1/users/login', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ email, password }),
+//     });
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
+//     const data = await res.json(); // Convert response to JSON
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
+//     if (!res.ok) {
+//       throw new Error(data.message || 'Login failed');
+//     }
+
+//     if (data.status === 'success') {
+//       alert('Logged in successfully!');
+//       window.setTimeout(() => {
+//         location.assign('/');
+//       }, 1500);
+//     }
+//   } catch (error) {
+//     alert(error.message || 'An error occurred');
+//   }
+// };
