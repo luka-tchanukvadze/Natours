29d8c32b0d09a8a878d9c7dba49268e39283e597
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-02-10 21:37:51 +0400

    rendering error pages

--- public/js/login.js
+++ public/js/login.js
@@ -32,7 +32,6 @@ export const logout = async () => {
 
     if (res.data.status === 'success') location.reload(true);
   } catch (error) {
-    console.log(error);
     showAlert('error', 'Error loggin out! try again.');
   }
 };
