c602553756257b2037d23f47410e9dddcb60a9fe
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-02-12 20:12:05 +0400

    saving image name to database

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,8 +44,6 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
   // 1) Create error if uesr POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -58,6 +56,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Filtered out unwanted field names that are not allowed to be updated
   const filteredBody = filterOBJ(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 2) Update user document
   // body.role: 'admin' can not be allowed
