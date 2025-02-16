538f3486131cf8f398066148c0940e273c955961
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-02-16 22:56:32 +0400

    init

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,8 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const AppError = require('../utils/appError');
+// const AppError = require('../utils/appError');
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
-const factory = require('./handlerFactory');
+// const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
